b7b06f9bb2eb259a15d7e2da229226449d4ff0bc
anash08 <104541645+anash08@users.noreply.github.com>
2023-06-03 01:32:14 +0530

    Update server.js

--- serverapp/server.js
+++ serverapp/server.js
@@ -24,19 +24,7 @@ app.set('port', port);
 
 const server = http.createServer(app);
 
-async function myFunction() {
-  // Other code...
-  
-  const browser = await puppeteer.launch();
 
-  // Use `await` for asynchronous operations within this function
-  // ...
-
-  await browser.close();
-}
-
-// Call the async function
-myFunction();
 
 
 /**
